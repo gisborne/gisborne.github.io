b192df174918c808c7984affd77586559f068794
Guyren Howe <guyren@relevantlogic.com>
2022-02-22 14:33:36 -0800

    Fix resume not redrawing

--- lib/macos/resume.dart
+++ lib/macos/resume.dart
@@ -1,13 +1,44 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:pdfx/pdfx.dart';
 import 'package:flutter/cupertino.dart';
 
 class ResumePage extends StatelessWidget {
-  final pdfPinchController = PdfControllerPinch(
-    document: PdfDocument.openAsset('assets/resume.pdf'),
-  );
+  static final _page = PdfDocument.openAsset('assets/resume.pdf');
+
 
   @override
   Widget build(BuildContext context) {
-      return PdfViewPinch(controller: pdfPinchController);
+      return Stack(
+        alignment: Alignment(1.0, -1.0),
+          children: <Widget>[
+            PdfViewPinch(controller: PdfControllerPinch(document: _page)),
+            _DownloadControl('assets/resume.pdf'),
+        ]
+    );
+  }
+}
+
+class _DownloadControl extends StatelessWidget {
+  final String path;
+  _DownloadControl(this.path, [Key? key]):
+      super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+        onPressed: download,
+        child: Icon(
+            Icons.download,
+            size: 32,
+          color: Colors.black26)
+    );
+  }
+
+  void download() {
+    var result = FilePicker.platform.pickFiles(allowMultiple: true);
+    result.then((f){
+
+    });
   }
 }
