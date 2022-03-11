803497a7edf2083e2893fd0d744d70d26816002d
Guyren Howe <guyren@relevantlogic.com>
2022-03-11 13:43:01 -0800

    About box with licenses

--- lib/macos/about.dart
+++ lib/macos/about.dart
@@ -9,18 +9,33 @@ class About extends StatelessWidget {
       alignment: Alignment.topCenter,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SimpleStyledText('''A digital Resume for Guyren G Howe.
-        
+        child: Column(
+          children: [
+            SimpleStyledText('''A digital Resume for Guyren G Howe.
+            
 I made this because I saw the Flutter team was hiring, and I would be great for a role there, and this seemed like a good approach :-).
-        
+            
 Also as an exercise and as a change from the humdrum Word resume.
-        
+            
 I don’t consider myself a Flutter expert, but I do alright. If your team needs some subset of a web/database person who can be a Flutter expert pretty quickly, and can also do training and writing, get in touch.
 
 Tested on MacOS, iOS and Web. Should work on all platforms.
 
 <link href="https://github.com/gisborne/resume">github.com/gisborne/resume</link>
 <link href="mailto:guyren@relevantlogic.com>guyren@relevantlogic.com</link>'''),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: CupertinoButton(
+                child: Text('More…'),
+                onPressed: (() => showAboutDialog(
+                    context: context,
+                  children: [
+                    SimpleStyledText('Written to persuade someone to hire me to do Flutter, because it is the future.')
+                  ])),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
